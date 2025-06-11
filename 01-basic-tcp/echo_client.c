930ae39c140264ca6e0511e817fe6e024f2a11aa
MattSoo <sooyeonglih1996@gmail.com>
2025-06-11 21:07:11 +0800

    feat(01): add basic TCP client and server

--- 01-basic-tcp/echo_client.c
+++ 01-basic-tcp/echo_client.c
@@ -8,3 +8,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3 && argc != 2) {
+    fprintf(stderr,"Usage: %s [host] [port]\n", argv[0]);
+    return 1;
+  }
+
+  char *host = NULL;
+  char *port = "12345";
+  if (argc == 3) {
+    host = argv[1];
+    port = argv[2];
+  } else if (argc == 2)
+  {
+    host = argv[1];
+  }
+
+  struct addrinfo hint;
+  hint.ai_family = AF_UNSPEC;
+  hint.ai_socktype = SOCK_STREAM;
+  hint.ai_protocol = IPPROTO_TCP;
+  hint.ai_flags = AI_PASSIVE;
+
+  struct addrinfo *bind_address;
+  if (getaddrinfo(host, port, &hint, &bind_address)) {
+    perror("getaddrinfo() failed");
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  int socket_listen = socket(bind_address->ai_family, 
+    bind_address->ai_socktype, bind_address->ai_protocol);
+  if (socket_listen < 0) {
+    perror("socket() failed");
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  if (connect(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen) < 0) {
+    perror("connect() failed");
+    close(socket_listen);
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  freeaddrinfo(bind_address);
+
+  while (1)
+  {
+    char message[1024];
+    printf("Enter message: ");
+    if (!fgets(message, sizeof(message), stdin)) break;
+    message[strcspn(message, "\n")] = 0;
+
+    send(socket_listen, message, sizeof(message), 0);
+    recv(socket_listen, message, sizeof(message), 0);
+    printf("Received: %s\n", message);
+  }
+    
+
+  close(socket_listen);
+  return 0;
+}
--- 01-basic-tcp/echo_server.c
+++ 01-basic-tcp/echo_server.c
@@ -8,3 +8,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr,"Usage: %s [port]\n", argv[0]);
+    return 1;
+  }
+
+  char *port = argv[1];
+
+  struct addrinfo hint;
+  hint.ai_family = AF_UNSPEC;
+  hint.ai_socktype = SOCK_STREAM;
+  hint.ai_protocol = IPPROTO_TCP;
+  hint.ai_flags = AI_PASSIVE;
+
+  struct addrinfo *bind_address;
+  if (getaddrinfo(NULL, port, &hint, &bind_address)) {
+    perror("getaddrinfo() failed");
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  int socket_listen = socket(bind_address->ai_family, 
+    bind_address->ai_socktype, bind_address->ai_protocol);
+  if (socket_listen < 0) {
+    perror("socket() failed");
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen) < 0) {
+    perror("bind() failed");
+    close(socket_listen);
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  freeaddrinfo(bind_address);
+
+  if (listen(socket_listen, 10) < 0) {
+    perror("listen() failed");
+    close(socket_listen);
+    freeaddrinfo(bind_address);
+    return 1;
+  }
+
+  return 0;
+}
